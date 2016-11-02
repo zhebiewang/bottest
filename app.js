dad857e2c3f8621364d3e45f8e5e69a7782d42f9
zhebiewang <fwangjie@gmail.com>
2016-11-02 17:45:03 +0800

    bot test codd
    bot test codd

---
+++ app.js
@@ -0,0 +1,28 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: '7f899a97-e787-4ec3-bde5-31271700893f',
+    appPassword: 'F2GfofJZgPoNyKktq7YoCO3'
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
